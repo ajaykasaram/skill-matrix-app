d2a942c7fcf0b7f7adda74f83017631ce22a48ad
ajaykasaram <ajay.kasaram@outlook.com>
2025-02-10 21:41:04 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
 })
